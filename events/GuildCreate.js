2b9c5af48341fec9f6123ebc4b7408438af6db2c
Szonszczyk <mmmchw@gmail.com>
2022-09-20 15:33:10 +0200

    delete not working files

--- events/GuildCreate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'guildCreate',
-	once: false,
-	execute(client, guild) {
-		client.logger.log('[events]guildCreate', `New guild => ${guild.name} with ${guild.memberCount} members`);
-	},
-};
--- events/GuildDelete.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'guildDelete',
-	once: false,
-	execute(client, guild) {
-		client.logger.log('[events]guildDelete', `Removed guild => ${guild.name} with ${guild.memberCount} members`);
-	},
-};
